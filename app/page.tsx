1253803725ea02423b1323b0467fc1a61378a19c
hy57in <gywls00100@gmail.com>
2024-10-06 23:38:04 +0900

    chore: page 소개 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Hyojin Kim
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`안녕하세요, 프론트엔드 개발자 김효진입니다!`}
       </p>
       <div className="my-8">
         <BlogPosts />
