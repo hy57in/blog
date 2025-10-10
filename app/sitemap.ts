30e1ca3ae47193c248f77d167612f6d5c95857fd
hyojin <hyojin@hyojinui-MacBookAir.local>
2025-10-10 17:01:27 +0900

    fix: sitemap baseUrl update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,6 @@
 import { getBlogPosts } from 'app/blog/utils'
 
-// TODO: 실제 배포 URL로 변경하세요
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://hyojin.dev'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
